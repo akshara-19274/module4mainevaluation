d12170b9692205071a65f60ef0070c44efebbcc2
akshara-19274 <gakshara19274@gmail.com>
2026-01-31 21:02:54 +0530

    Debug 1

--- fleetmanagement/routes/vehicle.routes.js
+++ fleetmanagement/routes/vehicle.routes.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
-const {addVehicle,assignVehicle,getVehicles}=require('../controllers/vehicle.controller');
-const rateLimit=require('../middlewares/rateLimit');
+const {addVehicle,assignDriver,getVehicles}=require('../controllers/vehicle.controller');
+const rateLimit=require('../middlewares/rateLimiter');
 router.post('/add',rateLimit,addVehicle);
 router.patch('/assign-driver/:vehicleId',assignDriver);
 router.get('/:vehicleId',getVehicles);
