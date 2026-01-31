0638a37afb5c1dd531faf5aa71cb3f6bf7126d1b
akshara-19274 <gakshara19274@gmail.com>
2026-01-31 21:11:39 +0530

    Debug 2

--- fleetmanagement/middlewares/logger.js
+++ fleetmanagement/middlewares/logger.js
@@ -1,6 +1,10 @@
 const fs=require('fs');
 module.exports=(req,res,next)=>{
     const log='${req.method} ${req.url} ${new Date().toISOString()}\n';
-    fs.appendFile('logs.txt,log');
+    try {
+        fs.appendFileSync('log.txt',log);
+    } catch (err) {
+        console.error('Failed to write log:',err);
+    }
     next();
 };
